06505a341880fe29dbfeb8170be0106eeb297a9f
Benjamin megret <benji@MacBook-Pro-de-Benjamin.local>
2021-03-17 22:32:55 +0100

    mongo

--- app.js
+++ app.js
@@ -11,19 +11,6 @@ require('./config')(app);
 const session = require('express-session');
 const MongoStore = require('connect-mongo').default;
 
-app.use(session({
-    secret: 'NotMyAge',
-    saveUninitialized: false, 
-    resave: false, 
-    cookie: {
-      maxAge: 1000*60*60*24// is in milliseconds.  expiring in 1 day
-    },
-    store: new MongoStore({
-      mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/TechChallenge",
-      ttl: 60*60*24, // is in seconds. expiring in 1 day
-    })
-}));
-
 const allRoutes = require('./routes');
 app.use('/api', allRoutes);
 
